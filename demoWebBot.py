4399bd1fee91587b76d2f60c59291d35c7377db4
rupeshkumar8825 <111901045@smail.iitpkd.ac.in>
2023-06-16 22:56:28 +0530

    Made a simple set up for learning how to make a simple web bots

---
+++ demoWebBot.py
@@ -0,0 +1,3 @@
+# in this we will make a demo webbot for learning purpose using selenium for this purpose 
+print("Hi this is a simple code for learning how to make a bot and inktegrate with the already existing website for this purpose and for learning purpose and for earning purpose")
+
